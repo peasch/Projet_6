d07034757a1b94903e71f810c9a819119e8d35c0
Pierre-Alain Schaming <peaschaming@gmail.com>
2020-04-23 19:32:33 +0200

    premiers ajouts spring de config

---
+++ src/main/java/com/lade/Dao/mysqlDataSource.java
@@ -0,0 +1,23 @@
+package com.lade.Dao;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("?????????????")
+public class mysqlDataSource {
+    @Bean
+    public DataSource mysqlDataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/ladedb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true");
+        dataSource.setUsername("root");
+        dataSource.setPassword("ocpizza");
+
+        return dataSource;
+    }
+}
